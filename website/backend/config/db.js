62edd986a7cbf7d87fde26d4dc77c84452207aea
garimachandna <gchandna4@gmail.com>
2024-01-11 01:47:25 +0530

    troubleshooting uri- handling .env

--- website/backend/config/db.js
+++ website/backend/config/db.js
@@ -1,9 +1,18 @@
 mongoose = require("mongoose");
 colors = require("colors");
+path = require("path");
+dotenv = require("dotenv");
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
 const connectDB = async () => {
   try {
-    console.log("connecting to database", process.env.MONGO_URL);
+    console.log(
+      "connecting to database",
+      path.resolve(__dirname, "../.env"),
+      process.env.MONGO_URL
+    );
     const conn = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,

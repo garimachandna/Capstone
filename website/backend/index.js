6350ee5f2e2ffef3991e4e0a93c71c8c48962eda
garimachandna <gchandna4@gmail.com>
2024-01-11 19:15:28 +0530

    figuring cors error

--- website/backend/index.js
+++ website/backend/index.js
@@ -18,8 +18,11 @@ var allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") res.send(200);
-  else next();
+  if (req.method === "OPTIONS") {
+    console.log("OPTIONS SUCCESS");
+
+    res.send(200);
+  } else next();
 };
 
 //rest object

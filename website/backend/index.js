f7c3a15d695cbd58348ae8c480a9a2ab869d3e32
garimachandna <gchandna4@gmail.com>
2024-01-11 18:59:18 +0530

    figuring cors error

--- website/backend/index.js
+++ website/backend/index.js
@@ -14,8 +14,18 @@ dotenv.config();
 console.log(process.env.MONGO_URL);
 connectDB();
 
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") res.send(200);
+  else next();
+};
+
 //rest object
 const app = express();
+
+app.use(allowCrossDomain);
 
 //middlewares
 app.use(

b7859842007caa67214d99218a8d1cc20fc93e2c
garimachandna <gchandna4@gmail.com>
2024-01-11 14:03:40 +0530

    updated vercel.json

--- website/backend/index.js
+++ website/backend/index.js
@@ -19,11 +19,12 @@ const app = express();
 
 //middlewares
 app.use(
-  cors({
-    origin: "https://shikayat-frontend.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true, // enable set cookie
-  })
+  cors()
+  //   {
+  //   origin: "https://shikayat-frontend.vercel.app",
+  //   methods: ["GET", "POST"],
+  //   credentials: true, // enable set cookie
+  // }
 );
 app.use(express.json());
 app.use(morgan("dev"));
